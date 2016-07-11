e24c1b208b627a055aac43000eabe2231d12eaf3
Juan Sidrach de Cardona Mora <jsidrach@ucsd.edu>
2016-07-10 22:48:25 -0700

    Open preferences

--- app/src/main/java/sneakycoders/visualreact/MainMenu.java
+++ app/src/main/java/sneakycoders/visualreact/MainMenu.java
@@ -2,6 +2,8 @@ package sneakycoders.visualreact;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+    }
+
+    public void openPreferences(View view) {
+        Intent intent = new Intent(MainMenu.this, Preferences.class);
+        startActivity(intent);
     }
 }
