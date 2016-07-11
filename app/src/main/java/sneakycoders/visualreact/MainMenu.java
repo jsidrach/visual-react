8621e672b09b3238511411980bf74db25a86427b
Juan Sidrach de Cardona Mora <jsidrach@ucsd.edu>
2016-07-10 17:58:17 -0700

    Bump min API to v16

--- app/src/main/java/sneakycoders/visualreact/MainMenu.java
+++ app/src/main/java/sneakycoders/visualreact/MainMenu.java
@@ -8,6 +8,6 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.main_menu);
     }
 }
