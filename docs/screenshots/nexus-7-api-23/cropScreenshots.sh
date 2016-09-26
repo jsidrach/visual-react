9f7c952592015973b1758a96fb660ca99272080d
Juan Sidrach de Cardona Mora <jsidrach@ucsd.edu>
2016-09-25 17:18:09 -0700

    Fix crop

--- docs/screenshots/nexus-7-api-23/cropScreenshots.sh
+++ docs/screenshots/nexus-7-api-23/cropScreenshots.sh
@@ -3,12 +3,12 @@
 # Requires ImageMagick
 
 # New height
-croppedHeight=2464
+croppedHeight=1824
 for file in *.png; do
     width=$(identify -format '%w' ${file})
     height=$(identify -format '%h' ${file})
     # Only crop uncropped images
-    if [ ${width} == 1200 ] && [ ${height} == 1824 ]; then
+    if [ ${width} == 1200 ] && [ ${height} == 1920 ]; then
         convert ${file} -crop ${width}x${croppedHeight}+0+0 ${file}
         printf "Cropped %s\n" ${file}
     fi
